c19a03f6f8e6707bced757bbae6dcdecb049766d
Luis Evilla <luisevilla588@gmail.com>
2024-02-15 19:09:20 -0500

    show avatar on navbar and logout button

--- apps/frontend/src/ui/components/Navbar.tsx
+++ apps/frontend/src/ui/components/Navbar.tsx
@@ -1,17 +1,24 @@
 import {
+  Avatar,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
   Link,
   Navbar,
   NavbarBrand,
   NavbarContent,
   NavbarItem,
 } from '@nextui-org/react';
+import { AuthContext } from '@ocmi/frontend/app/contexts/authContext';
+import { useContext } from 'react';
 
 export default function AdminNavbar() {
+  const { user, logout } = useContext(AuthContext);
+
   return (
     <Navbar isBordered>
-      <NavbarBrand>
-        PayPeo
-      </NavbarBrand>
+      <NavbarBrand>PayPeo</NavbarBrand>
       <NavbarContent>
         <NavbarItem>
           <Link href="/enterprise">Enterprise</Link>
@@ -24,6 +31,30 @@ export default function AdminNavbar() {
         <NavbarItem>
           <Link href="/payments-sheet">Payments Sheet</Link>
         </NavbarItem>
+
+        <Dropdown>
+          <DropdownTrigger>
+            <Avatar
+              isBordered
+              as="button"
+              className="transition-transform"
+              color="secondary"
+              name="Jason Hughes"
+              size="sm"
+              src={"https://i.pravatar.cc/150?u=a042581f4e29026024d"}
+              id="settings-button"
+            />
+          </DropdownTrigger>
+          <DropdownMenu aria-label="Profile Actions" variant="flat">
+            <DropdownItem key="profile" className="h-14 gap-2">
+              <p className="font-semibold">Signed in as</p>
+              <p className="font-semibold">{user?.email}</p>
+            </DropdownItem>
+            <DropdownItem key="logout" color="danger" onPress={logout}>
+              Log Out
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
       </NavbarContent>
     </Navbar>
   );
